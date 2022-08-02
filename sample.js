f8a32b0c8731b245fadd2f2cce1f338856d80899
thedream <markspencerjordan@gmail.com>
2022-08-03 00:36:32 +0800

    refactor-project

--- sample.js
+++ sample.js
@@ -1,36 +1,10 @@
-
-function aCounter(word) {
-    /* Takes in a word and returns the number of a's within that word. Counts
-    both lowercase (a) and uppercase (A). Your job is to translate the following
-    function to use a `for` loop instead of the `while` loop it is currently
-    using. */
-
-    /*
-    let index = 0;
-    let count = 0;
-    while (index < word.length) {
-        let char = word[index];
-        if (char === "a" || char === "A") {
-        count += 1;
-        }
-        index++;
-    }
-    return count;
-    */
-    // Your code here
-    let count = 0 ;
-    for(let index= 0; index<word.length; index++){
-        let char = word[index];
-
-        if (char === "a"){
-            count+=1;
-        }
-        else if(char === "A"){
-            count+=1;
+function returnSevens(max) {
+    for (let i = 0; i < max; i++) {
+        if (i % 7 === 0) {
+            console.log(i);
         }
 
     }
-    return count;
-};
+}
 
-console.log(aCounter("aaAAaaaAAaa"));
+console.log(returnSevens(49));
